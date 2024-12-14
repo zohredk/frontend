96f4520ed99ba3e1bc9e780aebdb3100a77efc55
zohredk <zohrehdk2000@gmail.com>
2024-12-14 18:51:48 +1100

    .

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,37 +1,36 @@
 import React, { useEffect } from "react";
-
 import { useDispatch, useSelector } from "react-redux";
-
 import { Row, Col } from "react-bootstrap";
-
 import Product from "../components/Product/Product";
-
 import { productListAction } from "../action/productAction";
 
 const Home = () => {
   const dispatch = useDispatch();
-
   const productList = useSelector((state) => state.productList);
-
-  const { loading, products } = productList;
+  const { loading, error, products } = productList;
 
   useEffect(() => {
     dispatch(productListAction());
   }, [dispatch]);
+
   return (
     <div>
       <h1>Products</h1>
       {loading ? (
-        <h2> Loading products...</h2>
+        <h2>Loading products...</h2>
+      ) : error ? (
+        <h2>{error}</h2>
       ) : (
         <Row>
-          {products.map((item) => {
-            return (
+          {products && products.length > 0 ? (
+            products.map((item) => (
               <Col key={item._id} sm={12} md={6} lg={4}>
                 <Product product={item} />
               </Col>
-            );
-          })}
+            ))
+          ) : (
+            <h2>No products found</h2>
+          )}
         </Row>
       )}
     </div>
